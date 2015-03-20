b76d013a101ef3625aa52d0615f606fa7a171d45
Meghav Mehta <meghavdmehta@gmail.com>
2015-03-20 16:18:59 -0700

    first commit

---
+++ application/controllers/time.php
@@ -0,0 +1,19 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Time extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->output->enable_profiler();
+	}
+
+	public function index()
+	{
+		date_default_timezone_set("America/Los_Angeles");
+		$this->load->view('index',array("timenow" => strftime("%X %Z"), "datenow" => strftime("%B %d %Y")));
+
+	}
+}
+
+//end of main controller
---
+++ application/views/index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+	<title>Time Display</title>
+	<style type="text/css">
+		.block{
+			margin: auto;
+			border: 1px solid black;
+			text-align: center;
+			width:350px;
+		}
+	</style>
+</head>
+<body>
+	<div class='block'>
+		<p>The current time is </p>
+		<h1><?= $timenow ?></h1>
+		<h2><?= $datenow ?></h2>
+	</div>
+
+</body>
+</html>
